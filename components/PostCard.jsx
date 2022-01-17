520abe73601a9c2e4d170ff9a7c88b27e8084c72
imronxz <imronxz@gmail.com>
2022-01-18 01:09:39 +0700

    04. Working with PostCard Blog

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -8,6 +8,7 @@ function PostCard({ post }) {
 
   return (
     <div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
+      {/* Judul/Title Post blog */}
       <div className="relative overflow-hidden shadow-md pb-80 mb-6">
         <img
           src={post.thumbnailImage.url}
@@ -16,10 +17,9 @@ function PostCard({ post }) {
         />
       </div>
       <h1 className="transition duration-700 text-center mb8 cursor-pointer hover:text-pink-600 text-3xl font-semibold">
-        <Link key={post.id} href={`/post/${post.slug}`}>
-          {post.title}
-        </Link>
+        <Link href={`/post/${post.slug}`}>{post.title}</Link>
       </h1>
+      {/* Admin Post, photo dan nama  */}
       <div className="block lg:flex text-center items-center justify-center mb-8 w-full">
         <div className="flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8">
           <img
@@ -33,6 +33,7 @@ function PostCard({ post }) {
             {post.admin.nama}
           </p>
         </div>
+        {/* logo svg */}
         <div className="font-medium text-gray-700">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -48,7 +49,21 @@ function PostCard({ post }) {
               d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
             />
           </svg>
+          {/* Penanggalan dengan moment */}
+          <span>{moment(post.createdAt).format('DD - MMMM - YYYY')}</span>
         </div>
+      </div>
+      {/* Kutipan blog post */}
+      <p className="text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8">
+        {post.kutipan}
+      </p>
+      {/* BUTTON */}
+      <div className="text-center">
+        <Link href={`/post/${post.slug}`}>
+          <span className="transition duration-500 tansform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">
+            Lanjutkan Membaca
+          </span>
+        </Link>
       </div>
     </div>
   );
